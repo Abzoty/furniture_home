09709e8cb3870748f8995c39b7b95c337677b543
Abzoty <abzootyy@gmail.com>
2025-08-24 13:39:17 +0300

    Remove unused Favorite model import from api.php

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\FavoriteController;
 use App\Http\Controllers\ProductController;
-use App\Models\Favorite;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
